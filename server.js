7ddf4012f3233a38895094bf590f2b3fd5e4c959
sanjaysanzu <sanjayyadlapalli5@gmail.com>
2024-11-05 20:27:37 +0530

    changed for initial

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
-app.use(express.json())
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+app.use(express.json());
 
-
-const userRouter = require('./routes/users')
-app.use('/users', userRouter)
-
-const productRouter = require('./routes/products')
-app.use('/product', productRouter)
-
-const companiesRouter = require('./routes/companies')
-app.use('/company', companiesRouter)
-
-const assessmentsRouter = require('./routes/assessments')
-app.use('/assessments', assessmentsRouter)
-
-const testRouter = require('./routes/test')
-app.use('/test', testRouter)
+app.get("/mine", async (req, res) => {
+  try {
+    const mine = {
+      firstName: "Sanjay",
+      lastName: "Y",
+      age: 25,
+      place: "Vinukonda, Palnadu, Andhra Pradesh",
+    };
+    res.send(mine)
+  } catch (e) {
+    res.status(401).send({ error_message: e.message });
+  }
+});
 
 const initializeDBAndServer = () => {
-    const PORT = 4001
-    app.listen(PORT, () => {
-        console.log(`Server running at ${PORT}`)
-    })
-    mongoose.set('strictQuery', false)
-    mongoose.connect('mongodb+srv://sanjayyadlapalli5:YiOuG0qGbrOpMx13@cluster0.feee3hr.mongodb.net/myFirstDb?retryWrites=true&w=majority', { useNewUrlParser: true })
-    const db = mongoose.connection
-    db.on('error', (error) => console.error(error))
-    db.once('open', () => console.log('Connected to Database'))
-}
-initializeDBAndServer()
+  const PORT = 4001;
+  app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`);
+  });
+};
+initializeDBAndServer();
